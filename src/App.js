3cb9a705eca336e9f1ebbcb0303a7b3feebb8776
Mafifiza <refilwemotaung2025@outlook.com>
2025-11-03 03:03:17 +0200

    Integrate all components in main app and download Bootstrap

--- src/App.js
+++ src/App.js
@@ -1,10 +1,27 @@
+import React from "react";
+import { ThemeProvider } from "./contexts/ThemeContext";
+import Header from "./components/Header/Header";
+import Player from "./components/Player/Player";
+import Playlist from "./components/Playlist/Playlist";
+import { sampleSongs } from "./utils/sampleData";
+import "./styles/global.css";
 import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <h1>Waveform Music Player</h1>
-    </div>
+    <ThemeProvider>
+      <div className="app">
+        <Header />
+        <div className="app-content">
+          <Player />
+          <Playlist
+            songs={sampleSongs}
+            currentSong={sampleSongs[0]}
+            onSelectSong={(song) => console.log("Selected:", song)}
+          />
+        </div>
+      </div>
+    </ThemeProvider>
   );
 }
 
